4efa88ffbcadab899c6c7844e65e87c26814f005
Apple <apple@Apples-MacBook-Pro-5.local>
2019-07-24 10:31:17 -0400

    first commit

---
+++ Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Day One Challenges - Printing Karlie Kloss
+//Variables: var first = "Karlie" + var last = "Kloss"
+
+//var first = "Karlie"
+//var last = "Kloss"
+
+//NUMBER 1
+//print(first + last)
+
+//NUMBER 2
+//print (last + first)
+
+//NUMBER 3
+//print (first + " " + last)
+
+//NUMBER 4
+//print (last + " " + first + " " + last + " " + first)
+
+//NUMBER 5
+//print ("I love" + " " + first)
+
+//Day One After Lunch Challenges - Doubles and Floats
+
+//we want to cast integer as a double so 4 --> 4.0
+//var integer = 4
+//5.0 is a double because of the decimal
+//var double = 5.0
+//integer*double
+
+//var a = 12
+//var b = 65
+//var c = 31
+//var d = 98
+
+//Print The Total of A-D
+//print (a + b + c + d)
+//Print Average
+//print ((a + b + c + d) / 4)
+
+//PART 1 - Variable or Constant --> Determmine if each item is a variable or constant?
+//Constant: something that never changes
+//Variable: never changes unless declared to change
+1) Name: constant
+2) Address: variable
+3) Birthday: constant
+4) Grade Level:variable
+5) Age: variable
+6) Eye Color: constant
+7) Favorite Color: variable
+
+//PART 2 - How Much Do I Get Paid?
+//Karina = 10.25 / hour --> in total she works 20 hours --> how much money will she make?
+
+var double = 10.25
+print (double * 20)
+
+//PART 3 - Sing Happy Birthday to Your Favorite Celeb
+
+
